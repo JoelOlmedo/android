fbab2d395b07662507bc53cf0d507aced9e5bdbb
JoelOlmedo <99646086+JoelOlmedo@users.noreply.github.com>
2023-09-19 11:00:49 +0200

    fds

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.androidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.androidapp.ui.theme.AndroidAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +25,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PrintToScreen("juel","dsf")
+                    Column() {
+
+                        PrintToScreen("juel")
+                        PrintToScreen("fsdf")
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    val names = listOf("asda","sada","sada")
+
+                    for(name in names){
+                        
+                        PrintToScreen(name)
+                    }
                 }
             }
         }
@@ -29,22 +46,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PrintToScreen(name: String?,name2: String?, modifier: Modifier = Modifier) {
+fun PrintToScreen(name: String?, modifier: Modifier = Modifier) {
 
-    val name = name ?: run{
-
-        Log.e("NullError", "Name is Null")
-        return
-    }
-
-    val name2 = name2 ?: run {
-
-        Log.e("NullError", "Name is Null")
-        return
-    }
 
     Text(
-        text = "Hello ${name}! ${name2}",
+        text = "Hello ${name}!",
         modifier = modifier
     )
 }
